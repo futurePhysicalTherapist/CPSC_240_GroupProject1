878e71633dd4f94e85e419de6d55dfac35737239
KNK <KNK@10.213.189.204>
2017-11-06 13:07:28 -0500

    Testing commit for the first time

---
+++ src/cpsc_240_groupproject1/CPSC_240_GroupProject1.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpsc_240_groupproject1;
+
+/**
+ *
+ * @author KNK
+ */
+public class CPSC_240_GroupProject1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is my first commit!");
+        
+    }
+    
+}
