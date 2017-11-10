57eed1371e0bcf4c5f2f02224fbc3c9f852060b0
jackieschuster <jackieschuster@10.213.136.172>
2017-11-10 13:52:25 -0500

    Jackie added constructor, variables, getters, setters to User.java

--- src/cpsc_240_groupproject1/User.java
+++ src/cpsc_240_groupproject1/User.java
@@ -9,7 +9,33 @@ package cpsc_240_groupproject1;
  *
  * @author KNK
  */
-public class User 
-{
+public class User {
+    String eMail;
+    int creditCardNumber;
     
+    public User(){
+        eMail = null;
+        creditCardNumber = 0;
+    }
+    
+    public User(String uEMail, int uCCNum){
+        eMail = uEMail;
+        creditCardNumber = uCCNum;
+    }
+    
+    public void setEmail(String eMail) {
+        this.eMail = eMail;
+    }
+    
+    public void setCreditCardNumber(int creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+    
+    public String getEmail() {
+        return eMail;
+    }
+    
+    public int getCreditCardNumber() {
+        return creditCardNumber;
+    }
 }
