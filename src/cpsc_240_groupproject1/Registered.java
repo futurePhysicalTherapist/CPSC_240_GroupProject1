e851d71f2fc1b98b6dab4482efc34b78046553b6
KNK <KNK@10.213.187.40>
2017-11-13 12:50:10 -0500

    Added classes.

---
+++ src/cpsc_240_groupproject1/Registered.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cpsc_240_groupproject1;
+
+/**
+ *
+ * @author KNK
+ */
+public class Registered {
+    
+}
