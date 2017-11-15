3b6cb52edb1b9c02565a3bf4007ccc213d34116d
Cici22 <sjones26@mail.umw.edu>
2017-11-15 16:24:53 -0500

    Added setters and getter to the room class !!

--- src/cpsc_240_groupproject1/Room.java
+++ src/cpsc_240_groupproject1/Room.java
@@ -14,71 +14,129 @@ package cpsc_240_groupproject1;
  */
 public class Room 
 {
-    String floor; // Basement, first, second, third
-    int roomNumber;
-    boolean isAvailable;
+
+    int roomNum;
+    int capacity;
+    boolean smoking;
+    boolean reserved;
+    boolean handicapAccess;
+    double roomPrice;
+    String roomType;
 
     public Room()
     {        
-        floor = null;
-        roomNumber = 0;
-        isAvailable = false;  
+        roomNum = 0;
+        capacity = 0;
+        smoking = false;
+        reserved = false;
+        handicapAccess = false;
+        roomPrice = 3.50;
+        roomType = null;
+
     }
     
-    public Room(boolean avail, int roomNum, String floorName)
-    {
-        isAvailable = avail;
-        roomNumber = roomNum;
-        floor = floorName;
+    public Room(int roomNumber, int roomCapacity, boolean smoke, boolean reserve, boolean access, double price, String type  ){
+        roomNum = roomNumber;
+        capacity = roomCapacity;
+        smoking = smoke;
+        reserved = reserve;
+        handicapAccess = access;
+        roomPrice = price;
+        roomType = type;
     }
-    
     /**
-     * Have my getters
      * @return 
      */
-     public boolean getIsAvailable()
-    { 
-        return isAvailable; 
-    }
-    
-    public int getRoomNumber()
-    { 
-        return roomNumber; 
-    }
-    
-    public String getFloor()
+    public int getRoomNum()
     {
-        return floor;
+     return roomNum;   
     }
+    public int getCapacity()
+    {
+        return capacity;
+    }
+    public boolean getSmoking()
+    {
+        return smoking;
+    }
+    public boolean getReserved()
+    {
+        return reserved;
+    }
+    public boolean getHandicapAccess()
+    {
+        return handicapAccess;
+    }
+    public double getRoomPrice()
+    {
+        return roomPrice;
+    }
+    public String getRoomType()
+    {
+        return roomType;
+    }
+    /**
+     * Here are the setters!!!!
+     */
     
     /**
-     * Have my setters
-     * @param avail 
+     * @param roomNumber 
      */
-     public void setIsAvailable(boolean avail)
-     {
-         isAvailable = avail;
-     }
-     
-     public void setRoomNumber(int roomNum)
-     {
-         roomNumber = roomNum;
-     }
-     
-     public void setFloor(String floorName)
-     {
-         floor = floorName;
-     }
+    public void setRoomNum(int roomNumber)
+    {
+        roomNumber = roomNum;
+    }
     
+    //not sure i need a setter for capacity...
+    public void setCapacity(int roomCapacity)
+    {
+        roomCapacity = capacity;
+    }
+    /**
+     * 
+     * @param smoke 
+     */
+    public void setSmoking(boolean smoke)
+    {
+        smoke = smoking;
+    }
+    /**
+     * 
+     * @param reserve 
+     */
+    public void setReserved(boolean reserve)
+    {
+        reserve = reserved;
+    }
+        
+    //not sure i need a setter for handicapAccess...
+    public void setHandicapAccess(boolean access)
+    {
+        access = handicapAccess;
+    }
+    /**
+     * 
+     * @param price 
+     */
+    public void setRoomPrice(double price)
+    {
+        price = roomPrice;
+    }
+    public void setRoomType(String type)
+    {
+        type = roomType;
+    }
+    
+
+}
+
     
 
     
-    //public void addRoom()
-    //{
-        //room.add(addRoom);
-    //}
     
-    //public void setRoom()
+
+
     
     
-}
+
+ 
