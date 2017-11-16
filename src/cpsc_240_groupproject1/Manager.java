2e7751d42fd6f2494a15232baf10d9737c5062bc
ViceroySSB <31859153+ViceroySSB@users.noreply.github.com>
2017-11-16 15:42:13 -0500

    Finished Manager class

--- src/cpsc_240_groupproject1/Manager.java
+++ src/cpsc_240_groupproject1/Manager.java
@@ -9,6 +9,20 @@ package cpsc_240_groupproject1;
  *
  * @author KNK
  */
-public class Manager {
+public class Manager extends Registered{
     
+    String IDNum;
+    
+    public Manager(){
+        super(null,0,null,null,null,null,null);
+        IDNum = null;
+    }
+    public Manager(String eMail,int ccNum,String fName,String lName, String accNum,
+            String pass,String memStat,String id){
+        super(eMail,ccNum,fName,lName,accNum,pass,memStat);
+        IDNum = id;
+    }
+    
+    public String getIDNum(){ return IDNum; }
+    public void setIDNum(String id){ IDNum = id; }
 }
