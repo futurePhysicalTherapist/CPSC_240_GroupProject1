3925fdc84e4062dc56f747cccdf06c8bb8fe8953
ViceroySSB <31859153+ViceroySSB@users.noreply.github.com>
2017-11-16 15:00:00 -0500

    Finished Guest class

--- src/cpsc_240_groupproject1/Guest.java
+++ src/cpsc_240_groupproject1/Guest.java
@@ -9,6 +9,21 @@ package cpsc_240_groupproject1;
  *
  * @author KNK
  */
-public class Guest {
+public class Guest extends User{
     
+    String phoneNum;
+    
+    public Guest(){
+        super(null, 0);
+        phoneNum = null;
+    }
+    
+    public Guest(String email, int ccNum, String pNum){
+        super(email,ccNum);
+        phoneNum = pNum;
+    }
+    
+    public void setPhoneNum(String pNum){ phoneNum = pNum; }
+    
+    public String getPhoneNum(){ return phoneNum; }
 }
